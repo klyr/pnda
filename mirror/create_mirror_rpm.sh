03833de2cbe987567ea5e62398433b8be95b5d0e
trsmith2 <trsmith2@cisco.com>
2018-08-20 12:38:47 -0500

    Allow alternative root for RPM mirror
    PNDA-4902
    
    Thanks to @trsmith2 -

--- mirror/create_mirror_rpm.sh
+++ mirror/create_mirror_rpm.sh
@@ -1,5 +1,7 @@
-#!/bin/bash -ev
-export DISTRO=$(cat /etc/*-release|grep ^ID\=|awk -F\= {'print $2'}|sed s/\"//g)
+#!/bin/bash -evx
+
+export DISTRO=$(awk -F\= '/^ID=/ {print $2}' /etc/os-release | sed 's/"//g')
+export RELEASEVER=$(awk -F\= '/^VERSION_ID=/ {print $2}' /etc/os-release | sed 's/"//g')
 
 [[ -z ${MIRROR_BUILD_DIR} ]] && export MIRROR_BUILD_DIR=${PWD}
 [[ -z ${MIRROR_OUTPUT_DIR} ]] && export MIRROR_OUTPUT_DIR=${PWD}/mirror-dist
@@ -11,6 +13,7 @@ RPM_REPO_DIR=$MIRROR_OUTPUT_DIR/mirror_rpm
 [[ -z ${RPM_EXTRAS} ]] && export RPM_EXTRAS=rhui-REGION-rhel-server-extras
 [[ -z ${RPM_OPTIONAL} ]] && export RPM_OPTIONAL=rhui-REGION-rhel-server-optional
 RPM_EPEL=https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+RPM_EPEL_KEY=https://archive.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7
 MY_SQL_REPO=https://repo.mysql.com/yum/mysql-5.5-community/el/7/x86_64/
 MY_SQL_REPO_KEY=https://repo.mysql.com/RPM-GPG-KEY-mysql
 CLOUDERA_MANAGER_REPO=http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/${CLOUDERA_MANAGER_VERSION}/
@@ -22,8 +25,14 @@ SALT_REPO_KEY2=http://repo.saltstack.com/yum/redhat/7/x86_64/${SALTSTACK_REPO}/b
 [[ -z ${AMBARI_LEGACY_REPO} ]] && export AMBARI_LEGACY_REPO=http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/${AMBARI_LEGACY_VERSION}/ambari.repo
 [[ -z ${AMBARI_REPO_KEY} ]] && export AMBARI_REPO_KEY=http://public-repo-1.hortonworks.com/ambari/centos7/RPM-GPG-KEY/RPM-GPG-KEY-Jenkins
 
+if [[ -n "$YUM_ROOT" ]]; then
+    YUMROOTOPTS="--installroot $YUM_ROOT"
+    RPMROOTOPTS="--root $YUM_ROOT"
+fi
+
 yum install -y $RPM_EPEL || true
-yum install -y yum-utils
+yum install -y yum-utils createrepo
+
 yum-config-manager --enable $RPM_EXTRAS $RPM_OPTIONAL
 yum-config-manager --add-repo $MY_SQL_REPO
 yum-config-manager --add-repo $CLOUDERA_MANAGER_REPO
@@ -36,11 +45,14 @@ rm -rf $RPM_REPO_DIR
 mkdir -p $RPM_REPO_DIR
 
 cd $RPM_REPO_DIR
-cp /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7 $RPM_REPO_DIR
 if [ "x$DISTRO" == "xrhel" ]; then
 	# Not present on CentOS
 	cp /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release $RPM_REPO_DIR
 fi
+
+yum $YUMROOTOPTS --releasever=$RELEASEVER --downloaddir=$RPM_REPO_DIR -y install $DISTRO-release
+
+curl -LOJf $RPM_EPEL_KEY
 curl -LOJf $MY_SQL_REPO_KEY
 curl -LOJf $CLOUDERA_MANAGER_REPO_KEY
 curl -LOJf $SALT_REPO_KEY
@@ -48,19 +60,12 @@ curl -LOJf $SALT_REPO_KEY2
 curl -LOJf $AMBARI_REPO_KEY
 
 # import repo keys
-rpm --import *
+rpm $RPMROOTOPTS --import *
 
-
-yum install -y createrepo
-
-#TODO yumdownloader doesn't always seem to download the full set of packages, for instance if git is installed, it won't download perl
-# packages correctly maybe because git already installed them. repotrack is meant to be better but I couldn't get that working.
-# yumdownloader also doesn't set its exit code when a package is not found, so this scans the log output for this case and manually exits with an error
-(yumdownloader --resolve --archlist=x86_64 --destdir $RPM_REPO_DIR -x "*.i686" $RPM_PACKAGE_LIST 2>&1) | tee -a yum-downloader.log; cmd_result=${PIPESTATUS[0]} && if [ ${cmd_result} != '0' ]; then exit ${cmd_result}; fi
-if grep -q 'No Match for argument' "yum-downloader.log"; then
-    echo "missing rpm detected:"
-    echo $(cat yum-downloader.log | grep 'No Match for argument')
-    exit -1
+if [[ -n "$YUM_ROOT" ]]; then
+    cp /etc/yum.repos.d/* $YUM_ROOT/etc/yum.repos.d/
 fi
-rm yum-downloader.log
+
+yum --setopt=protected_multilib=false $YUMROOTOPTS --releasever=$RELEASEVER --downloadonly --downloaddir=$RPM_REPO_DIR -y install $RPM_PACKAGE_LIST
+
 createrepo --database $RPM_REPO_DIR
